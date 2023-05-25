f9a4a9cda57a8895d5de2852437d747760fbc70c
Iftee97 <tawsifmasrur@gmail.com>
2023-05-26 00:22:13 +0600

    created and used custom hook for cart persistence

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -3,7 +3,9 @@ import { useEffect, useState } from "react"
 export function useLocalStorage(key, initialValue) {
   const [value, setValue] = useState(() => {
     const jsonValue = localStorage.getItem(key)
-    if (jsonValue != null) return JSON.parse(jsonValue)
+    if (jsonValue != null) {
+      return JSON.parse(jsonValue)
+    }
 
     if (typeof initialValue === "function") {
       return initialValue()
