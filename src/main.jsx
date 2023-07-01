6ac9b763ef6c0f57894b1484422281e981d20830
Iftee97 <tawsifmasrur@gmail.com>
2023-07-02 04:41:37 +0600

    ...

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import './index.scss'
 import { UserContextProvider } from './contexts/user.context'
 import { CategoriesContextProvider } from './contexts/categories.context'
 import { CartContextProvider } from './contexts/cart.context'
 import { Elements } from '@stripe/react-stripe-js'
 import { loadStripe } from '@stripe/stripe-js'
+import App from './App'
+import './index.scss'
 
 // Make sure to call `loadStripe` outside of a component’s render to avoid
 // recreating the `Stripe` object on every render.
